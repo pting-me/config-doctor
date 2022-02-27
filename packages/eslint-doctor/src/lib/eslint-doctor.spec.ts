912b256b332c3294def1f9a9fbd91ec53d0eff1d
Philip Ting <philipjting@yahoo.com>
2022-02-26 17:11:40 -0800

    added more tests and fixed typescript-eslint extension issues

--- packages/eslint-doctor/src/lib/eslint-doctor.spec.ts
+++ packages/eslint-doctor/src/lib/eslint-doctor.spec.ts
@@ -1,6 +1,10 @@
 import { main } from './eslint-doctor';
 
 describe('eslint-doctor', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should run without failure', async () => {
     const consoleSpy = jest.spyOn(console, 'log');
     const testDirectory = expect
@@ -9,5 +13,49 @@ describe('eslint-doctor', () => {
     const testCasesDirectory = testDirectory + '../../fixtures/file-type/';
     await main(testCasesDirectory);
     expect(consoleSpy).toHaveBeenCalledWith('0 issues found.');
+  });
+
+  it('should detect extra prefixes', async () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    const testDirectory = expect
+      .getState()
+      .testPath.replace(/(.+)\/([^/]+)/, '$1/');
+    const testCasesDirectory =
+      testDirectory + '../../fixtures/extension/prefixes/';
+    await main(testCasesDirectory);
+    expect(consoleSpy).toHaveBeenCalledWith('2 issues found.');
+  });
+
+  it('should detect conflicting style extensions', async () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    const testDirectory = expect
+      .getState()
+      .testPath.replace(/(.+)\/([^/]+)/, '$1/');
+    const testCasesDirectory =
+      testDirectory + '../../fixtures/extension/style-extensions/';
+    await main(testCasesDirectory);
+    expect(consoleSpy).toHaveBeenCalledWith('1 issues found.');
+  });
+
+  it('should detect prettier not last', async () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    const testDirectory = expect
+      .getState()
+      .testPath.replace(/(.+)\/([^/]+)/, '$1/');
+    const testCasesDirectory =
+      testDirectory + '../../fixtures/extension/prettier-last/';
+    await main(testCasesDirectory);
+    expect(consoleSpy).toHaveBeenCalledWith('1 issues found.');
+  });
+
+  it('should detect ts extension after recommended', async () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    const testDirectory = expect
+      .getState()
+      .testPath.replace(/(.+)\/([^/]+)/, '$1/');
+    const testCasesDirectory =
+      testDirectory + '../../fixtures/extension/ts-after/';
+    await main(testCasesDirectory);
+    expect(consoleSpy).toHaveBeenCalledWith('1 issues found.');
   });
 });
--- packages/eslint-doctor/src/lib/eslint-doctor.ts
+++ packages/eslint-doctor/src/lib/eslint-doctor.ts
@@ -1,125 +1,131 @@
 import { Linter } from 'eslint';
+import debugOrig from 'debug';
+
 import { loadConfig } from './load-config';
 
-console.log('\nESLint Doctor\n\n');
-
-const issues: string[] = [];
-
-const getConfigFromFile = async (fileDirectory = './') => {
-  const config = await loadConfig(fileDirectory);
-
-  if (!config) {
-    throw new Error('Unable to parse ESLint config.');
-  }
-
-  return config;
-};
-
-const checkPrefixesThenRemove = (extensions: string[]) => {
-  const removePrefix = (extensions: string) => {
-    const matches = extensions.match(/^eslint-config-(.+)/);
-    if (matches && matches[1]) {
-      issues.push(
-        `Extension "${matches[0]}" can be renamed to "${matches[1]}"\n\n`
-      );
-      // TODO: add autofix
-      return matches[1];
-    }
-    return extensions;
-  };
-
-  return extensions.map(removePrefix);
-};
-
-const checkConflictingStyleExtensions = (extensions: string[]) => {
-  const styleExtensions = ['airbnb', 'standard', 'google', 'xo'];
-
-  const usedStyleExtensions = extensions.filter((extension) => {
-    return styleExtensions.includes(extension);
-  });
-
-  if (usedStyleExtensions.length > 1) {
-    let message =
-      'There are too many style enforcing extensions.\n' +
-      'We recommend only using one of the following:\n';
-    usedStyleExtensions.forEach((extensions) => {
-      message += `* ${extensions}\n`;
-    });
-    message += '\n';
-    issues.push(message);
-  }
-};
-
-const checkPrettierIsLast = (extensions: string[]) => {
-  const hasPrettier = extensions.includes('prettier');
-  if (hasPrettier) {
-    const prettierIsLast = extensions[extensions.length - 1] === 'prettier';
-    if (!prettierIsLast) {
-      issues.push('The Prettier extension should always be placed last.\n\n');
-      // TODO: add autofix
-    }
-  }
-};
-
-const checkTypescriptExtensionIsAfterEslintRecommended = (
-  extensions: string[]
-) => {
-  const recommendedIndex = extensions.indexOf('eslint:recommended');
-  const tsExtensions = [
-    'plugin:@typescript-eslint/all',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ];
-
-  if (recommendedIndex !== -1) {
-    tsExtensions.forEach((extension) => {
-      // if recommended comes after current extension
-      if (recommendedIndex > extensions.indexOf(extension)) {
-        issues.push(
-          `'${extension}' is supposed to come after 'eslint:recommended'`
-        );
-      }
-    });
-  }
-};
-
-const checkExtensions = (config: Linter.Config) => {
-  // TODO: check overrides
-
-  // only need to check 'extends' if it's an array
-  if (!(config.extends && typeof config.extends === 'object')) {
-    return;
-  }
-
-  // Check: 'eslint-config-' prefix is unnecessary
-  const parsedExtensions = checkPrefixesThenRemove(config.extends);
-
-  // Check: config should not have more than one style enforcing extension
-  checkConflictingStyleExtensions(parsedExtensions);
-
-  // Check: 'prettier' extension must be last in extension list
-  checkPrettierIsLast(parsedExtensions);
-
-  // TODO: load package.json
-
-  // Check: @typescript-eslint/eslint-recommended is not required
-  //   if @typescript-eslint version >= 3.0.0
-  //   and there's another @typescript-eslint extension enabled
-  // TODO: implement
-  // TODO: add autofix
-
-  // Check: @typescript-eslint plugins should all have the same version
-  // TODO: implement
-  // TODO: add autofix
-
-  // Check: @typescript-eslint should always come after eslint:recommended
-  checkTypescriptExtensionIsAfterEslintRecommended(parsedExtensions);
-};
+const debug = debugOrig('eslint-doctor:eslint-doctor');
 
 const main = async (fileDirectory = './') => {
+  console.log('\nESLint Doctor\n\n');
+
+  const issues: string[] = [];
+
+  const getConfigFromFile = async (fileDirectory = './') => {
+    const config = await loadConfig(fileDirectory);
+
+    if (!config) {
+      throw new Error('Unable to parse ESLint config.');
+    }
+
+    return config;
+  };
+
+  const checkPrefixesThenRemove = (extensions: string[]) => {
+    const removePrefix = (extensions: string) => {
+      const matches = extensions.match(/^eslint-config-(.+)/);
+      if (matches && matches[1]) {
+        issues.push(
+          `Extension "${matches[0]}" can be renamed to "${matches[1]}"\n\n`
+        );
+        // TODO: add autofix
+        return matches[1];
+      }
+      return extensions;
+    };
+
+    return extensions.map(removePrefix);
+  };
+
+  const checkStyleExtensions = (extensions: string[]) => {
+    const styleExtensions = ['airbnb', 'standard', 'google', 'xo'];
+
+    const usedStyleExtensions = extensions.filter((extension) => {
+      return styleExtensions.includes(extension);
+    });
+
+    if (usedStyleExtensions.length > 1) {
+      let message =
+        'There are too many style enforcing extensions.\n' +
+        'We recommend only using one of the following:\n';
+      usedStyleExtensions.forEach((extensions) => {
+        message += `* ${extensions}\n`;
+      });
+      message += '\n';
+      issues.push(message);
+    }
+  };
+
+  const checkPrettierIsLast = (extensions: string[]) => {
+    const hasPrettier = extensions.includes('prettier');
+    if (hasPrettier) {
+      const prettierIsLast = extensions[extensions.length - 1] === 'prettier';
+      if (!prettierIsLast) {
+        issues.push('The Prettier extension should always be placed last.\n\n');
+        // TODO: add autofix
+      }
+    }
+  };
+
+  const checkTsIsAfterRecommended = (extensions: string[]) => {
+    const recommendedIndex = extensions.indexOf('eslint:recommended');
+    const tsExtensions = [
+      'plugin:@typescript-eslint/all',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ];
+
+    if (recommendedIndex !== -1) {
+      tsExtensions.forEach((extension) => {
+        // if recommended comes after current extension
+        if (
+          extensions.indexOf(extension) > -1 &&
+          recommendedIndex > extensions.indexOf(extension)
+        ) {
+          issues.push(
+            `'${extension}' is supposed to come after 'eslint:recommended'`
+          );
+        }
+      });
+    }
+  };
+
+  const checkExtensions = (config: Linter.Config) => {
+    // TODO: check overrides
+
+    // only need to check 'extends' if it's an array
+    if (!(config.extends && typeof config.extends === 'object')) {
+      return;
+    }
+
+    // Check: 'eslint-config-' prefix is unnecessary
+    const parsedExtensions = checkPrefixesThenRemove(config.extends);
+
+    // Check: config should not have more than one style enforcing extension
+    checkStyleExtensions(parsedExtensions);
+
+    // Check: 'prettier' extension must be last in extension list
+    checkPrettierIsLast(parsedExtensions);
+
+    // TODO: load package.json
+
+    // Check: @typescript-eslint/eslint-recommended is not required
+    //   if @typescript-eslint version >= 3.0.0
+    //   and there's another @typescript-eslint extension enabled
+    // TODO: implement
+    // TODO: add autofix
+
+    // Check: @typescript-eslint plugins should all have the same version
+    // TODO: implement
+    // TODO: add autofix
+
+    // Check: @typescript-eslint should always come after eslint:recommended
+    checkTsIsAfterRecommended(parsedExtensions);
+  };
+
   const config = await getConfigFromFile(fileDirectory);
   checkExtensions(config);
+
   issues.forEach((issue) => {
     console.log(issue);
   });
