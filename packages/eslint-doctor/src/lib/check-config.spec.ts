21c7ca03ad496dbf2e2e58f244e6fe9a3f6e7b5d
Philip Ting <philipjting@yahoo.com>
2022-03-06 06:58:00 -0800

    rewrote tests to include control cases

--- packages/eslint-doctor/src/lib/check-config.spec.ts
+++ packages/eslint-doctor/src/lib/check-config.spec.ts
@@ -13,73 +13,117 @@ describe('eslint-doctor', () => {
   });
 
   it('should detect extra prefixes', () => {
-    const { parsedExtensions, issues } = checkPrefixesThenRemove([
-      'eslint-config-airbnb',
-      'eslint-config-prettier',
-    ]);
+    const control = ['airbnb', 'prettier'];
+    const variable = ['eslint-config-airbnb', 'eslint-config-prettier'];
 
+    const { parsedExtensions: controlParsedExtensions, issues: controlIssues } =
+      checkPrefixesThenRemove(control);
+    expect(controlIssues.length).toBe(0);
+    expect(controlParsedExtensions).toStrictEqual(control);
+
+    const { parsedExtensions, issues } = checkPrefixesThenRemove(variable);
     expect(issues.length).toBe(2);
-    expect(parsedExtensions).toStrictEqual(['airbnb', 'prettier']);
+    expect(parsedExtensions).toStrictEqual(control);
   });
 
   it('should detect conflicting style extensions', () => {
-    const issues = checkStyleExtensions(['airbnb', 'google']);
+    const control = ['airbnb'];
+    const variable = ['airbnb', 'google'];
+
+    const controlIssues = checkStyleExtensions(control);
+    expect(controlIssues.length).toBe(0);
+
+    const issues = checkStyleExtensions(variable);
     expect(issues.length).toBe(1);
   });
 
-  it('should detect prettier not last', () => {
-    const issues = checkPrettierIsLast(['prettier', 'eslint:recommended']);
+  it('should detect when prettier is not in last position', () => {
+    const control = ['eslint:recommended', 'prettier'];
+    const variable = ['prettier', 'eslint:recommended'];
+
+    const controlIssues = checkStyleExtensions(control);
+    expect(controlIssues.length).toBe(0);
+
+    const issues = checkPrettierIsLast(variable);
     expect(issues.length).toBe(1);
   });
 
   it('should warn when @typescript-eslint/all is used', () => {
-    const issues = checkTsAll(['plugin:@typescript-eslint/all']);
+    const control: string[] = [];
+    const variable = ['plugin:@typescript-eslint/all'];
+
+    const controlIssues = checkTsAll(control);
+    expect(controlIssues.length).toBe(0);
+
+    const issues = checkTsAll(variable);
     expect(issues.length).toBe(1);
   });
 
-  it('should detect ts extension after recommended', () => {
-    const issues = checkTsIsAfterRecommended([
+  it('should detect when ts extensions are after eslint-recommended', () => {
+    const control = [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ];
+    const variable = [
       'plugin:@typescript-eslint/eslint-recommended',
       'plugin:@typescript-eslint/recommended',
       'plugin:@typescript-eslint/recommended-requiring-type-checking',
       'eslint:recommended',
-    ]);
+    ];
+
+    const controlIssues = checkTsIsAfterRecommended(control);
+    expect(controlIssues.length).toBe(0);
+
+    const issues = checkTsIsAfterRecommended(variable);
     expect(issues.length).toBe(3);
   });
 
   it("should warn when ts version >= 3.0.0 and doesn't need extra eslint-recommended reset", () => {
-    const issues = checkTsResetExtension(
-      [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      { devDependencies: { '@typescript-eslint/eslint-plugin': '5.0.0' } }
-    );
+    const packageJson = {
+      devDependencies: { '@typescript-eslint/eslint-plugin': '5.0.0' },
+    };
+
+    const control = [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ];
+    const variable = [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ];
+
+    const controlIssues = checkTsResetExtension(control, packageJson);
+    expect(controlIssues.length).toBe(0);
+
+    const issues = checkTsResetExtension(variable, packageJson);
     expect(issues.length).toBe(1);
   });
 
-  it('should warn when ts version >= 3.0.0 and eslint-recommended comes after', () => {
-    const issues = checkTsResetExtension(
-      [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-      ],
-      { devDependencies: { '@typescript-eslint/eslint-plugin': '5.0.0' } }
-    );
-    expect(issues.length).toBe(1);
-  });
+  it('should warn when ts version < 3.0.0 and @typescript-eslint/eslint-recommended comes after other ts extensions', () => {
+    const packageJson = {
+      devDependencies: { '@typescript-eslint/eslint-plugin': '2.0.0' },
+    };
 
-  it('should warn when ts version < 3.0.0 and eslint-recommended comes after', () => {
-    const issues = checkTsResetExtension(
-      [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-      ],
-      { devDependencies: { '@typescript-eslint/eslint-plugin': '2.0.0' } }
-    );
+    const control = [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+    ];
+    const variable = [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+    ];
+
+    const controlIssues = checkTsResetExtension(control, packageJson);
+    expect(controlIssues.length).toBe(0);
+
+    const issues = checkTsResetExtension(variable, packageJson);
     expect(issues.length).toBe(1);
   });
 });
